165f4313b805a2b67ef93fa81fc8b10326d8b385
jtpoplin <jason.poplin@gmail.com>
2026-06-04 11:24:30 +0200

    Added shooting feature

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
 from circleshape import CircleShape
+from shot import Shot
 import pygame
 
 class Player(CircleShape):
@@ -34,9 +35,18 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
     
     def move(self, dt):
         unit_vector = pygame.Vector2(0, 1)
         rotated_vector = unit_vector.rotate(self.rotation)
         rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
         self.position += rotated_with_speed_vector
+    
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        forward = pygame.Vector2(0, 1)
+        rotated = forward.rotate(self.rotation)
+        fast = rotated * PLAYER_SHOOT_SPEED
+        shot.velocity = fast
